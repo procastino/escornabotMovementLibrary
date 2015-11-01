032d71482f38adc9e3307ae41e699a4540fc6bf4
procastino <tuchodefresa@gmail.com>
2015-11-01 18:21:27 +0100

    libreria funcional

--- Escornabot.cpp
+++ Escornabot.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 #include "Escornabot.h"
 
-Escornabot::Escornabot(){
 #define STEPPERS_MOTOR_RIGHT_IN1 5
 #define STEPPERS_MOTOR_RIGHT_IN2 4
 #define STEPPERS_MOTOR_RIGHT_IN3 3
@@ -12,16 +11,29 @@ Escornabot::Escornabot(){
 #define STEPPERS_MOTOR_LEFT_IN4 6
 #define RETARDO_PASO 1000
 
+const static uint8_t step_pattern[] = 
+  {    B00001, B00011, B00010, B00110, B00100, B01100, B01000, B01001 }; 
+
+int8_t index_left, index_right;
+int8_t inc_motor_esq , inc_motor_der;
+
+Escornabot::Escornabot(){
+pinMode(STEPPERS_MOTOR_LEFT_IN1,OUTPUT);
+pinMode(STEPPERS_MOTOR_LEFT_IN2,OUTPUT);
+pinMode(STEPPERS_MOTOR_LEFT_IN3,OUTPUT);
+pinMode(STEPPERS_MOTOR_LEFT_IN4,OUTPUT);
+pinMode(STEPPERS_MOTOR_RIGHT_IN1,OUTPUT);
+pinMode(STEPPERS_MOTOR_RIGHT_IN2,OUTPUT);
+pinMode(STEPPERS_MOTOR_RIGHT_IN3,OUTPUT);
+pinMode(STEPPERS_MOTOR_RIGHT_IN4,OUTPUT);
+pinMode(10,OUTPUT);
+index_left=0;
+index_right=0;
 }
 
 void Escornabot::motor_step(int8_t inc_left, int8_t inc_right)
 {
   uint8_t pattern;
-  index_left=0;
-  index_right=0;
-  const static uint8_t step_pattern[] = 
-  {    B00001, B00011, B00010, B00110, B00100, B01100, B01000, B01001 };
-  
   if(inc_left==0) pattern=0; else pattern=step_pattern[index_left];
   digitalWrite(STEPPERS_MOTOR_LEFT_IN1, bitRead(pattern, 0));
   digitalWrite(STEPPERS_MOTOR_LEFT_IN2, bitRead(pattern, 1));
@@ -56,7 +68,7 @@ void Escornabot::anda(int pasos){
     inc_motor_der=-1;
   }  
   for(int contpasos=0; contpasos<pasos; contpasos++){
-    motor_step(inc_motor_esq,inc_motor_der);
+     motor_step(inc_motor_esq,inc_motor_der);
    }
 }
 
@@ -78,6 +90,13 @@ void Escornabot::xira(int pasos){
 
 void Escornabot::para(int tempo){
   delay(tempo);
+}
+
+void Escornabot::pitaTest(int tempo) {
+   digitalWrite(10,HIGH);
+   delay(tempo);
+   digitalWrite(10,LOW);
+   delay(tempo*200);
 }
 
 
--- Escornabot.h
+++ Escornabot.h
@@ -11,9 +11,8 @@ class Escornabot {
     void anda(int pasos);
     void xira(int pasos);
     void para(int tempo);
+    void pitaTest(int tempo);
     void motor_step(int8_t inc_left, int8_t inc_right);
-    const static uint8_t step_pattern[];
-    int8_t index_left, index_right;
-    int8_t inc_motor_esq , inc_motor_der;
+    
 };
 #endif
